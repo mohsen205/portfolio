5e8534405a0e35fc6cde0cb9c75593150aa49193
mohsen205 <mohsenmnassri7@gmail.com>
2022-04-09 10:07:43 +0200

    first commit

---
+++ components/underConstruction/index.js
@@ -0,0 +1,11 @@
+const index = () => {
+    return (
+        <div className="text-center " style={{ padding: "250px 0px 250px 0px" }}>
+            <div className="display-1">
+                This Page Is Under Construcation
+            </div>
+        </div>
+    )
+}
+
+export default index
